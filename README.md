03ddbbfc28652724eef6a8d067f0a693b945330e
jkp <Jishil.KP@amadeus.com>
2021-04-05 12:16:26 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,38 +1,57 @@
 
-# react-native-react-native-sound-toast
+# react-native-sound-toast
+
+A react native module to show native toast message, play sound and adjust system volume. Currently it works only on Android.
+
+## Features
+
+- Show Toast
+- Play sound & Adjust system volume
 
 ## Getting started
 
-`$ npm install react-native-react-native-sound-toast --save`
+`$ npm install react-native-sound-toast --save`
 
 ### Mostly automatic installation
 
-`$ react-native link react-native-react-native-sound-toast`
-
-### Manual installation
-
-
-#### Android
-
-1. Open up `android/app/src/main/java/[...]/MainActivity.java`
-  - Add `import com.reactlibrary.RNReactNativeSoundToastPackage;` to the imports at the top of the file
-  - Add `new RNReactNativeSoundToastPackage()` to the list returned by the `getPackages()` method
-2. Append the following lines to `android/settings.gradle`:
-  	```
-  	include ':react-native-react-native-sound-toast'
-  	project(':react-native-react-native-sound-toast').projectDir = new File(rootProject.projectDir, 	'../node_modules/react-native-react-native-sound-toast/android')
-  	```
-3. Insert the following lines inside the dependencies block in `android/app/build.gradle`:
-  	```
-      compile project(':react-native-react-native-sound-toast')
-  	```
-
+`$ react-native link react-native-sound-toast`
 
 ## Usage
 ```javascript
-import RNReactNativeSoundToast from 'react-native-react-native-sound-toast';
+import RNReactNativeSoundToast from 'react-native-sound-toast';
 
 // TODO: What to do with the module?
 RNReactNativeSoundToast;
+
+RNNativeSoundToast.showToastAtCenter("Hello World");
+
+RNNativeSoundToast.playSound("my_sound","wav");
+
+RNNativeSoundToast.increaseSound();
+
 ```
-  
+
+## Toast API
+
+- showToast(message)
+- showLongToast(message)
+- showShortToast(message)
+- showToastAtTop(message)
+- showToastAtCenter(message)
+
+## Sound API
+
+First you'll need to add audio files to your project.
+
+Save your sound clip files under the directory android/app/src/main/res/raw. Note that files in this directory must be lowercase and underscored (e.g. my_file_name.wav) and that subdirectories are not supported by Android
+
+- playSound(filename, type)
+- increaseSound()
+- decreaseSound()
+- muteSound()
+- unMuteSound()
+
+## Note
+
+This library is created for learning purpose, so please don't expect regular maintenance.
+
--- android/src/main/java/com/reactlibrarysoundtoast/RNReactNativeSoundToastModule.java
+++ android/src/main/java/com/reactlibrarysoundtoast/RNReactNativeSoundToastModule.java
@@ -2,12 +2,17 @@
 package com.reactlibrarysoundtoast;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.view.Gravity;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.io.File;
 
 public class RNReactNativeSoundToastModule extends ReactContextBaseJavaModule {
 
@@ -55,6 +60,66 @@ public class RNReactNativeSoundToastModule extends ReactContextBaseJavaModule {
     Toast toast= Toast.makeText(context,text, Toast.LENGTH_SHORT);
     toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 0);
     toast.show();
+  }
+
+  @ReactMethod
+  public void playSound(String name, String type) {
+    MediaPlayer mediaPlayer;
+    Context context = getReactApplicationContext();
+    int soundResID = context.getResources().getIdentifier(name, "raw", context.getPackageName());
+
+    if (soundResID > 0) {
+      mediaPlayer = MediaPlayer.create(context, soundResID);
+    } else {
+      mediaPlayer = MediaPlayer.create(context, getUriFromFile(name, type));
+    }
+    try {
+      if (mediaPlayer.isPlaying()) {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = MediaPlayer.create(context, soundResID);
+      } mediaPlayer.start();
+    } catch(Exception e) {
+      Toast.makeText(context,"Sound file not found",Toast.LENGTH_SHORT).show();
+    }
+  }
+
+  @ReactMethod
+  public void increaseSound() {
+    Context context = getReactApplicationContext();
+    AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+  }
+
+  @ReactMethod
+  public void decreaseSound() {
+    Context context = getReactApplicationContext();
+    AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
+  }
+
+  @ReactMethod
+  public void muteSound() {
+    Context context = getReactApplicationContext();
+    AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    audioManager.adjustVolume(AudioManager.ADJUST_MUTE, AudioManager.FLAG_SHOW_UI);
+  }
+
+  @ReactMethod
+  public void unMuteSound() {
+    Context context = getReactApplicationContext();
+    AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+    audioManager.adjustVolume(AudioManager.ADJUST_UNMUTE, AudioManager.FLAG_SHOW_UI);
+  }
+
+  private Uri getUriFromFile(String name, String type) {
+    String folder = getReactApplicationContext().getFilesDir().getAbsolutePath();
+    String file = name + "." + type;
+    File ref = new File(folder + "/" + file);
+    if (ref.exists()) {
+      ref.setReadable(true, false);
+    }
+    return Uri.parse("file://" + folder + "/" + file);
   }
 
 }
